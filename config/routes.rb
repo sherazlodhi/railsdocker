e41bd840af3fac53160ca99c5ddafe70c1c41a2a
Martin Artnik <artoartnik@gmail.com>
2020-06-16 11:24:24 +0200

    Add locations endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :weather_reports, only: [:index, :update]
+  resources :locations, only: :index
 end
